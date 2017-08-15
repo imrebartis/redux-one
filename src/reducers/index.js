b00290ddf030c8a0fc7435d4f76cb499bb13fa99
imrebartis <imbartis@gmail.com>
2017-08-15 07:28:04 +0300

    selectBook returns an action, reducer_active_book.js created for handling the 'Book_SELECTED' case

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducers_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  //any key added to the combined reducer (here: this index.js)
+  //ends up as a key in the global state
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
